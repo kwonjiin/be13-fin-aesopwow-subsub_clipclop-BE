b631b368eb253d1e5a5ddfb8076ebb056ebea78d
blooper20 <yjw127588@gmail.com>
2025-05-08 14:03:42 +0900

    [유재우] fix/refactor: 로거 선언 부분 수정

--- src/main/java/com/aesopwow/subsubclipclop/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aesopwow/subsubclipclop/global/exception/GlobalExceptionHandler.java
@@ -2,7 +2,8 @@ package com.aesopwow.subsubclipclop.global.exception;
 
 import com.aesopwow.subsubclipclop.domain.dbinfo.exception.DbInfoNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    Logger log;
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DbInfoNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleDbInfoNotFoundException(DbInfoNotFoundException ex) {
