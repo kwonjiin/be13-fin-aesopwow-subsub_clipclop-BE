4a1b3fd7bd39f75047f62ee1d17fe3e7e2d7354d
2HEEJIN <remember2076@gmail.com>
2025-05-09 11:09:44 +0900

    [이희진] 마이페이지 스웨거 관련 어노테이션 추가

--- src/main/java/com/aesopwow/subsubclipclop/controller/UserController.java
+++ src/main/java/com/aesopwow/subsubclipclop/controller/UserController.java
@@ -6,9 +6,15 @@ import com.aesopwow.subsubclipclop.domain.user.dto.UserUpdateRequestDTO;
 import com.aesopwow.subsubclipclop.domain.user.service.UserService;
 import com.aesopwow.subsubclipclop.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +26,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/staffs")
 @Tag(name = "User", description = "유저 관련 API")
 @RequiredArgsConstructor
 public class UserController {
@@ -31,10 +38,28 @@ public class UserController {
     private final UserService userService;
 
     // 직원 추가
-    @PostMapping("/api/staffs")
+    @PostMapping("/staffs")
+    @Operation(summary = "직원 추가", description = "관리자 계정으로 직원을 추가합니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "INTERNAL SERVER ERROR",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )
+    })
     public ResponseEntity<BaseResponseDto<String>> addStaff(
-            @RequestParam Long adminUserNo,
-            @RequestParam String staffEmail
+            @RequestParam @Valid Long adminUserNo,
+            @RequestParam @Valid @Email String staffEmail
     ) {
         userService.addStaff(adminUserNo, staffEmail);
 
@@ -45,20 +70,56 @@ public class UserController {
 
 
     // 직원정보 조회
-    @GetMapping("/api/staffs")
-    public ResponseEntity<List<UserResponseDTO>> getStaffList(@RequestParam Long adminUserNo) {
-        List<User> staffList = userService.getStaffList(adminUserNo);
+    @GetMapping("/staffs")
+    @Operation(summary = "직원 정보 조회", description = "관리자 계정으로 직원을 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "INTERNAL SERVER ERROR",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )
+    })
+    public ResponseEntity<BaseResponseDto<List<UserResponseDTO>>> getStaffList(@RequestParam @Valid Long adminUserNo) {
+            List<User> staffList = userService.getStaffList(adminUserNo);
 
-        List<UserResponseDTO> result = staffList.stream()
-                .map(UserResponseDTO::from)
-                .toList();
+            List<UserResponseDTO> result = staffList.stream()
+                    .map(UserResponseDTO::from)
+                    .toList();
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(new BaseResponseDto<>(HttpStatus.OK, result));
     }
 
+
     // 직원정보 수정
-    @PutMapping("/users/{userNo}")
-    @Operation(summary = "직원 정보 수정", description = "직원이름, 부서명, 비밀번호를 수정합니다.")
+    @PutMapping("/staffs/{userNo}")
+    @Operation(summary = "직원 정보 수정", description = "관리자 계정으로 직원이름, 부서명, 비밀번호를 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "INTERNAL SERVER ERROR",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )
+    })
     public ResponseEntity<BaseResponseDto<String>> updateUser(
             @PathVariable Long userNo,
             @RequestBody UserUpdateRequestDTO userUpdateRequestDTO) {
@@ -69,7 +130,25 @@ public class UserController {
     }
 
     // 직원정보 삭제
-    @DeleteMapping("/api/staffs/{userNo}")
+    @DeleteMapping("/staffs/{userNo}")
+    @Operation(summary = "직원 삭제", description = "관리자 계정으로 직원을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "INTERNAL SERVER ERROR",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )
+    })
     public ResponseEntity<BaseResponseDto<String>> deleteStaff(@PathVariable Long userNo) {
         userService.deleteStaff(userNo);
         return ResponseEntity.ok(new BaseResponseDto<>(HttpStatus.OK, "직원이 삭제되었습니다."));
