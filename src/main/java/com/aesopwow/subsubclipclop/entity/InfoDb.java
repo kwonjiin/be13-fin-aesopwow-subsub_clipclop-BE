1dfeba052138a7794e6cff9b0d58c5c6faadfbf4
blooper20 <yjw127588@gmail.com>
2025-05-09 16:48:03 +0900

    [유재우] fix/refactor: 어노테이션 수정

--- src/main/java/com/aesopwow/subsubclipclop/entity/InfoDb.java
+++ src/main/java/com/aesopwow/subsubclipclop/entity/InfoDb.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -13,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
