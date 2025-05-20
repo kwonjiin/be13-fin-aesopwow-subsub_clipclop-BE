02a60b4dd3a582e19df6033f3f836684d0887f08
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-05-20 12:39:02 +0900

    [권지민] 배포테스트44

--- scripts/chmod.sh
+++ scripts/chmod.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# 전체 app 디렉토리 소유권 변경
+chown -R ec2-user:ec2-user /home/ec2-user/app
+
+# scripts 폴더 내 쉘 스크립트들 소유권과 실행권한 설정
 chown ec2-user:ec2-user /home/ec2-user/app/scripts/*.sh
 chmod +x /home/ec2-user/app/scripts/*.sh
