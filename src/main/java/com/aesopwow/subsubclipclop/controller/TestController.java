08fe82db8fa132ade5a44fce66883b3da1ae0a83
Hyeonjunnn <dev_sklg0602@naver.com>
2025-05-20 15:25:16 +0900

    [송현준] 로그인 세션 테스트 중(완료X)

---
+++ src/main/java/com/aesopwow/subsubclipclop/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.aesopwow.subsubclipclop.controller;
+
+import com.aesopwow.subsubclipclop.domain.user.dto.UserResponseDTO;
+import com.aesopwow.subsubclipclop.entity.CustomUserDetails;
+import com.aesopwow.subsubclipclop.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping()
+    public ResponseEntity<UserResponseDTO> getTest(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        User user = customUserDetails.getUser();
+        UserResponseDTO userResponseDTO = UserResponseDTO.from(user);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+}
