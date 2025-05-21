6b91a2efd9456e2c2c0534f31cec862b84d65eff
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-05-21 17:24:34 +0900

    [권지민] 배포테스트68

--- src/main/java/com/aesopwow/subsubclipclop/config/SwaggerConfig.java
+++ src/main/java/com/aesopwow/subsubclipclop/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "PROD Spring API",
+                title = "Spring API",
                 description = "OTT 구독 고객관리 서비스 API 입니다",
                 version = "v0.0.1"
         )
