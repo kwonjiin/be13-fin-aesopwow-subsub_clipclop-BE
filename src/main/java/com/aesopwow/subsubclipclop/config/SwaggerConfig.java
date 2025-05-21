98bdcc717ba75d3a7aa92e35bb6b2c67bb18eee9
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-05-21 09:14:51 +0900

    [권지민] 배포테스트59

--- src/main/java/com/aesopwow/subsubclipclop/config/SwaggerConfig.java
+++ src/main/java/com/aesopwow/subsubclipclop/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.aesopwow.subsubclipclop.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +15,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server().url("https://api.dagudok-service.com:8010");
+
         return new OpenAPI()
+                .addServersItem(server)
                 .info(new Info()
                         .title(title)
                         .version("0.0.2")
                         .description("Spring API입니다..1"));
+
     }
 }
